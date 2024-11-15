14e744fab14cb04a89bc61a84af1f1eef727b108
anisimov-vs <st129629@student.spbu.ru>
2024-11-15 13:19:32 +0300

    Update resource.h

--- src/video_core/amdgpu/resource.h
+++ src/video_core/amdgpu/resource.h
@@ -293,15 +293,15 @@ struct Image {
     }
 
     bool NeedsNormalizationPatch() const {
-        //return ((GetNumberFmt() == AmdGpu::NumberFormat::Unorm) && (AmdGpu::NameOf(image.GetDataFmt()) == "Format8_8_8_8"));
+        
         if (GetNumberFmt() == AmdGpu::NumberFormat::Unorm ||
             GetNumberFmt() == AmdGpu::NumberFormat::Snorm) {
-            //std::cout << "Format: " << AmdGpu::NameOf(GetDataFmt()) << "\n";
+            std::cout << "Format: " << AmdGpu::NameOf(GetDataFmt()) << "\n";
             switch (GetDataFmt()) {
             case DataFormat::Format32:
             case DataFormat::Format32_32:
             case DataFormat::Format32_32_32:
-            case DataFormat::Format8_8_8_8://Format32_32_32_32:
+            case DataFormat::Format32_32_32_32:
                 return true;
             default:
                 return false;
